474ee3117e1775d429a6ff93f09658fd1527c9ef
muhammadAjiSolehudin <muhammadajisolehudin@gmail.com>
2024-05-22 22:20:24 +0700

    add seed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 import carsRoutes from './api/carRouter';
-import customersRoutes from './api/customerRouter'
 
 const router = Router();
 
 router.use(carsRoutes);
 
-router.use(customersRoutes);
 
 export default router;
